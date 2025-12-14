cef3c826ca19dbecf59727bb627d81e24aeae35e
Baraah mo <baraahelase47@gmail.com>
2025-12-14 10:41:22 +0300

    commit for merge

--- h.dart
+++
@@ -1,7 +0,0 @@
-c:\Users\s62ht\Desktop\uni\itcs444_project\lib/services/background_notification_service.dart:      await createAdminNotification(
-c:\Users\s62ht\Desktop\uni\itcs444_project\lib/services/donation_service.dart:      await _notificationService.sendAdminNotification(
-c:\Users\s62ht\Desktop\uni\itcs444_project\lib/services/equipment_service.dart:  await createAdminNotification(
-c:\Users\s62ht\Desktop\uni\itcs444_project\lib/services/notification_service.dart:Future<void> createAdminNotification({
-c:\Users\s62ht\Desktop\uni\itcs444_project\lib/services/notification_service.dart:  Future<void> sendAdminNotification({
-c:\Users\s62ht\Desktop\uni\itcs444_project\lib/services/reservation_service.dart:     await _notificationService.sendAdminNotification(
-c:\Users\s62ht\Desktop\uni\itcs444_project\lib/services/reservation_service.dart:        await _notificationService.sendAdminNotification(
